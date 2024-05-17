607b12f1f053087aada92808eca22638218c3541
Aldi Prahasta <aldidwiki@gmail.com>
2024-05-17 15:34:37 +0700

    add intent gallery functionality

--- app/src/main/java/com/aldiprahasta/storyapp/presentation/addstory/AddStoryActivity.kt
+++ app/src/main/java/com/aldiprahasta/storyapp/presentation/addstory/AddStoryActivity.kt
@@ -1,15 +1,52 @@
 package com.aldiprahasta.storyapp.presentation.addstory
 
+import android.net.Uri
 import android.os.Bundle
+import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import com.aldiprahasta.storyapp.databinding.ActivityAddStoryBinding
+import timber.log.Timber
 
 class AddStoryActivity : AppCompatActivity() {
     private lateinit var binding: ActivityAddStoryBinding
+    private var currentImageUri: Uri? = null
+
+    private val launcherGallery = registerForActivityResult(
+            ActivityResultContracts.PickVisualMedia()
+    ) { uri: Uri? ->
+        if (uri != null) {
+            currentImageUri = uri
+            showImage()
+        } else {
+            Timber.d("Photo Picker", "No media selected")
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityAddStoryBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        setupListeners()
+    }
+
+    private fun setupListeners() {
+        binding.apply {
+            btnGallery.setOnClickListener {
+                startGallery()
+            }
+        }
+    }
+
+    private fun startGallery() {
+        launcherGallery.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly))
+    }
+
+    private fun showImage() {
+        currentImageUri?.let {
+            Timber.d("Image URI", "showImage: $it")
+            binding.imgStory.setImageURI(it)
+        }
     }
 }
