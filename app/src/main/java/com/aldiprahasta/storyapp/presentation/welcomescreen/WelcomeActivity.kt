28a633c31e7a6485511cf00c1a8a0591af2081cd
Aldi Prahasta <aldidwiki@gmail.com>
2024-05-17 14:41:51 +0700

    add logic if logged in

--- app/src/main/java/com/aldiprahasta/storyapp/presentation/welcomescreen/WelcomeActivity.kt
+++ app/src/main/java/com/aldiprahasta/storyapp/presentation/welcomescreen/WelcomeActivity.kt
@@ -3,12 +3,20 @@ package com.aldiprahasta.storyapp.presentation.welcomescreen
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
 import com.aldiprahasta.storyapp.databinding.ActivityWelcomeBinding
+import com.aldiprahasta.storyapp.presentation.home.HomeActivity
 import com.aldiprahasta.storyapp.presentation.login.LoginActivity
 import com.aldiprahasta.storyapp.presentation.register.RegisterActivity
+import com.aldiprahasta.storyapp.utils.MyPreferences
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import org.koin.android.ext.android.inject
 
 class WelcomeActivity : AppCompatActivity() {
     private lateinit var binding: ActivityWelcomeBinding
+    private val myPreferences by inject<MyPreferences>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,16 +27,26 @@ class WelcomeActivity : AppCompatActivity() {
     }
 
     private fun setupListeners() {
-        binding.apply {
-            btnLogin.setOnClickListener {
-                Intent(this@WelcomeActivity, LoginActivity::class.java).also {
-                    startActivity(it)
-                }
-            }
+        lifecycleScope.launch {
+            val isLoggedIn = myPreferences.getTokenFromDataStore().isNotEmpty()
 
-            btnSignup.setOnClickListener {
-                Intent(this@WelcomeActivity, RegisterActivity::class.java).also {
-                    startActivity(it)
+            withContext(Dispatchers.Main) {
+                binding.btnLogin.setOnClickListener {
+                    val intent = if (isLoggedIn) {
+                        Intent(this@WelcomeActivity, HomeActivity::class.java).also {
+                            finish()
+                        }
+                    } else {
+                        Intent(this@WelcomeActivity, LoginActivity::class.java)
+                    }
+
+                    startActivity(intent)
+                }
+
+                binding.btnSignup.setOnClickListener {
+                    Intent(this@WelcomeActivity, RegisterActivity::class.java).also {
+                        startActivity(it)
+                    }
                 }
             }
         }
